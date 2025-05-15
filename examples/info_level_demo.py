5baf3764a79ce3666d740f8310552f35813049f6
Marco Del Pin <marco.delpin@gmail.com>
2025-05-15 15:05:43 +0200

    Docs: Clarify PROGRESS level suppression in info_level_demo.py

--- examples/info_level_demo.py
+++ examples/info_level_demo.py
@@ -3,12 +3,17 @@ import logging
 
 def main():
     logger = tracecolor("demo")
-    logger.setLevel(logging.INFO)  # Set level to INFO
+    logger.setLevel(logging.INFO)  # Set level to INFO (numeric value 20)
+    # Messages with a level lower than INFO (20) will not be displayed.
 
-    logger.trace("This is a TRACE message (should NOT appear)")
-    logger.debug("This is a DEBUG message (should NOT appear)")
-    logger.info("This is an INFO message (should appear)")
-    logger.warning("This is a WARNING message (should appear)")
+    # These messages have levels lower than INFO and should NOT appear:
+    logger.trace("This is a TRACE message (level 5, should NOT appear)")
+    logger.debug("This is a DEBUG message (level 10, should NOT appear)")
+    logger.progress("This is a PROGRESS message (level 15, should NOT appear)")
+
+    # These messages have levels INFO or higher and should appear:
+    logger.info("This is an INFO message (level 20, should appear)")
+    logger.warning("This is a WARNING message (level 30, should appear)")
     logger.error("This is an ERROR message (should appear)")
     logger.critical("This is a CRITICAL message (should appear)")
 
