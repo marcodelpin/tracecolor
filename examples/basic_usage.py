25217bb85f7e8b26a3e8241adff14e717074cd44
Marco Del Pin <marco.delpin@gmail.com>
2025-05-15 15:08:08 +0200

    Style: Format example scripts for PEP 8 compliance

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -3,21 +3,12 @@ from tracecolor import tracecolor
 
 
 # Create a logger with the module name
-
 logger = tracecolor(__name__)
 
-
-
 # Log at different levels
-
 logger.trace("This is a trace message - very detailed info")
-
 logger.debug("This is a debug message - for developers")
-
 logger.info("This is an info message - general information")
-
 logger.warning("This is a warning message - potential issue")
-
 logger.error("This is an error message - something went wrong")
-
 logger.critical("This is a critical message - severe problem!")
