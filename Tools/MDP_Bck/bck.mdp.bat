88c87d6c5d6cde4d355ce5948fd47ed76c57db15
Marco Del Pin <marco.delpin@gmail.com>
2025-09-01 14:32:36 +0200

    Reorder backup operations: prioritize Users backup before Commesse

--- Tools/MDP_Bck/bck.mdp.bat
+++ Tools/MDP_Bck/bck.mdp.bat
@@ -2,9 +2,9 @@ s:\MDPUtils\Utils\Shell\LogRotate\logrotate.exe s:\MDPUtils\Utils\Shell\LogRotat
 
 set DST=\\192.168.71.9\Shared\Backup\Marco
 
-MDP_Bck.exe s:\Commesse %DST% .mdpignore
-
 MDP_Bck.exe s:\Users\Marco %DST% .mdpignore
+
+MDP_Bck.exe s:\Commesse %DST% .mdpignore
 
 set DST=\\192.168.71.9\Shared\Backup\MarcoC
 
